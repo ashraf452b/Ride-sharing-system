2e4f4f60818d1e53460903f7d8e294320313b0b7
ashraf452b <ashraf452b@gmail.com>
2026-03-02 20:00:59 +0600

    update

--- Ride sharing System/Ride.py
+++ Ride sharing System/Ride.py
@@ -1,6 +1,7 @@
-import datetime
+from datetime import datetime
+from vehicle import Car,Bike,Cng
 class Ride:
-    def __init__(self,start_location,end_location):
+    def __init__(self,start_location,end_location,vehicle_type):
         self.start_location=start_location
         self.end_location=end_location
         self.driver=None
@@ -8,6 +9,7 @@ class Ride:
         self.start_time=None
         self.end_time=None
         self.estimated_fare=None
+        self.vehicle_type=vehicle_type
 
     def set_driver(self,driver):
         self.driver=driver
@@ -19,6 +21,14 @@ class Ride:
         self.end_time=datetime.now()
         self.rider.wallet -= self.estimated_fare
         self.driver.wallet +=self.estimated_fare
+    
+    def calculate_fare(self,distance,vehicle):
+        per_km={
+            'car' : 100,
+            'bike': 60,
+            'cng'  : 80
+        }
+        return distance * per_km.get(vehicle)
 
     def __repr__(self):
         return f'Ride Details: started  {self.start_location} To  {self.end_location}'
@@ -33,13 +43,20 @@ class RideMatching:
     def __init__(self,drivers):
         self.drivers=drivers
         
-    def find_driver(self,ride_request):
+    def find_driver(self,ride_request,vehicle_type):
         if(len(self.drivers)>0):
             print('Looking for Drivers.....')
             driver=self.drivers[0]
-            ride=Ride(ride_request.rider.current_location,ride_request.end_location)
-            driver.accept_ride(ride)
+            if vehicle_type == 'Car':
+                vehicle=Car('car','C321A2R',100)
+            elif vehicle_type == 'Bike':
+                vehicle=Bike('Bike','BI047K3',80)
+            elif vehicle_type =='Cng':
+                vehicle=Cng('Cng','C65NG',30)
 
+            ride=Ride(ride_request.rider.current_location,ride_request.end_location,vehicle)
+            driver.accept_ride(ride)
+            return ride
 
             
                 
--- Ride sharing System/users.py
+++ Ride sharing System/users.py
@@ -1,4 +1,22 @@
 from abc import ABC, abstractmethod
+from ride import RideRequest,RideMatching
+class Ride_sharing:
+    def __init__(self,name):
+        self.name=name
+        self.drivers=[]
+        self.riders=[]
+        self.rides=[]
+    
+    def add_rider(self,rider):
+        self.riders.append(rider)
+
+    def add_driver(self,driver):
+        self.drivers.append(driver)
+    
+    def __repr__(self):
+        return f'Company Name {self.name} with {len(self.drivers)} Drivers and {len(self.rides)} Rides'
+    
+
 class User(ABC):
     def __init__(self,name,email,nid):
         self.name=name
@@ -6,7 +24,7 @@ class User(ABC):
         self.nid=nid
     
     @abstractmethod
-    def disply_profile(self):
+    def display_profile(self):
         raise NotImplementedError
     
 
@@ -17,7 +35,7 @@ class Rider(User):
         self.wallet=initial_amount
         self.current_location=current_location
 
-    def disply_profile(self):
+    def display_profile(self):
         print(f'Rider Name = {self.name}\nEmail = {self.email}')
     
     def load_cash(self,amount):
@@ -26,8 +44,12 @@ class Rider(User):
         else:
             print('Faild! Amount Less than zero')
     
-    def request_ride(self,ride_sharing,destination):
-        pass
+    def request_ride(self,ride_sharing,destination,vehicle_type):
+        ride_request=RideRequest(self,destination)
+        ride_matching=RideMatching(ride_sharing.drivers)
+        ride=ride_matching.find_driver(ride_request,vehicle_type)
+        self.current_ride=ride
+        print('YAY! We got a ride')
 
     def show_current_ride(self):
         print(self.current_ride)
@@ -40,8 +62,19 @@ class Driver(User):
         self.current_location=current_location
         self.wallet=0
 
-    def disply_profile(self):
+    def display_profile(self):
         print(f'Driver Name = {self.name}') 
 
     def accept_ride(self,ride):
+        ride.start_ride()
         ride.set_driver(self)
+
+    def reach_destination(self,ride):
+        ride.end_ride()
+
+
+
+
+
+
+    
